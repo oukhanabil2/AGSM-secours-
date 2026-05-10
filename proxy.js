e0c96f94d473f09476fa12ca8c462f35cc14ecf3
oukhanabil2 <oukhanabil2@gmail.com>
2026-05-10 17:33:42 +0100

    Delete proxy.js

--- proxy.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  if (req.method === 'OPTIONS') return res.status(200).end();
-  res.status(200).json({ message: 'Proxy OK' });
-}
